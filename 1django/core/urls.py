b50d9d19cd0e4320b48fb6f6c500597ead06d481
Rushi6767 <rushisathavara581998@gmail.com>
2026-04-15 12:00:00 -0400

    WIP: Improving Code Quality

--- 1django/core/urls.py
+++ 1django/core/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('create-one/', views.create_one),
-    # path('create-many/', views.create_many),
-    # path('get-all/', views.get_all),
-    # path('get-one/<int:id>/', views.get_one),
-    # path('update/<int:id>/', views.update_one),
-    # path('delete/<int:id>/', views.delete_one),
-    # path("orm/", views.orm),
-    path("data/", views.data_create),
+    path('create-one/', views.create_one),
+    path('create-many/', views.create_many),
+    path('get-all/', views.get_all),
+    path('get-one/<int:id>/', views.get_one),
+    path('update/<int:id>/', views.update_one),
+    path('delete/<int:id>/', views.delete_one),
+    path("orm/", views.orm),
+    
 ]
